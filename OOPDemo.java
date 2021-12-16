941e6bc192cf62994390a7fcf8ed47be47333010
unknown <you@example.com>
2021-12-16 09:11:43 +0530

    Commit Message

---
+++ OOPDemo.java
@@ -0,0 +1,9 @@
+class OOPDemo {
+     public static void main(String[] args) {
+	   OOPDemo oopDemo = new OOPDemo();
+	   oopDemo.demo();
+	   }
+	   void demo() {
+	   System.out.println("Hello");
+	   }
+	 }
